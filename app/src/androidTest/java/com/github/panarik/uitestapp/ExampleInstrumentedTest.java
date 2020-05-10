c66ad3cb2b15d3227ae1655f7daa58e8f67c60ce
panarik@yandex.ru <panarik@yandex.ru>
2020-05-10 23:23:47 +0300

    Test_MainActivity_ExampleInstrumentedTest

--- app/src/androidTest/java/com/github/panarik/uitestapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/github/panarik/uitestapp/ExampleInstrumentedTest.java
@@ -8,6 +8,14 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.junit.Rule;
+import androidx.test.rule.ActivityTestRule;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -17,6 +25,10 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -24,4 +36,10 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.github.panarik.uitestapp", appContext.getPackageName());
     }
+
+    @Test
+    public void clickButtonHome(){
+        onView(withId(R.id.navigation_home)).perform(click()).check(matches(isDisplayed()));
+    }
+
 }
