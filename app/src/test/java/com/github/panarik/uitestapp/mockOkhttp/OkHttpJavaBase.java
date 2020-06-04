365a399d24bb77ae6808fa3a8207634dc25dda0f
a.levin <a.levin@foodplexgroup.ru>
2020-06-04 17:03:08 +0300

    OkHttp separate mock tests on extend Class

--- app/src/test/java/com/github/panarik/uitestapp/mockOkhttp/OkHttpJavaBase.java
+++ app/src/test/java/com/github/panarik/uitestapp/mockOkhttp/OkHttpJavaBase.java
@@ -40,34 +40,7 @@ public class OkHttpJavaBase {
     }
 
 
-    @Test
-    public void testOkHttp() throws Exception {
 
-        //run Get Request
-        responseBody = getRequest(client, serverUrl);
-
-        //matcher
-        Assert.assertEquals("test body", responseBody); //body equals
-    }
-
-    @Test
-    public void testTimeouts() throws Exception {
-
-        //configure client for set timeout
-        client = new OkHttpClient.Builder()
-                .connectTimeout(20, TimeUnit.SECONDS)
-                .writeTimeout(20, TimeUnit.SECONDS)
-                .readTimeout(20, TimeUnit.SECONDS)
-                .callTimeout(20, TimeUnit.SECONDS)
-                .build();
-
-        //run Get Request
-        responseBody = getRequest(client, serverUrl);
-
-        //matcher
-        Assert.assertEquals("test body", responseBody); //body equals
-
-    }
 
     @After
     public void stopMockServer() throws Exception {
---
+++ app/src/test/java/com/github/panarik/uitestapp/mockOkhttp/OkHttpJavaTest.java
@@ -0,0 +1,40 @@
+package com.github.panarik.uitestapp.mockOkhttp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+public class OkHttpJavaTest extends OkHttpJavaBase {
+
+    @Test
+    public void testOkHttp() throws Exception {
+
+        //run Get Request
+        responseBody = getRequest(client, serverUrl);
+
+        //matcher
+        Assert.assertEquals("test body", responseBody); //body equals
+    }
+
+    @Test
+    public void testTimeouts() throws Exception {
+
+        //configure client for set timeout
+        client = new OkHttpClient.Builder()
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .callTimeout(20, TimeUnit.SECONDS)
+                .build();
+
+        //run Get Request
+        responseBody = getRequest(client, serverUrl);
+
+        //matcher
+        Assert.assertEquals("test body", responseBody); //body equals
+    }
+
+}
